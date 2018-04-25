214b81eea26b6f61d02031dfa045763a55954245
Antea Martín <anteamartinacinas@MacBook-Air-de-Antea.local>
2018-04-25 11:58:42 +0200

    Apañito del giorgi

--- models/Product.js
+++ models/Product.js
@@ -6,7 +6,7 @@ const productSchema = new Schema(
  name: String,
  description: String,
  price: Number,
- image: String
+ image: String,
   },
   {
     timestamps: {
--- models/User.js
+++ models/User.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+mongoose.plugin(schema => { schema.options.usePushEach = true });
 
 const userSchema = new Schema(
   {
     username: String,
     password: String,
     email: String,
-    cart: Array
+    cart: Array, 
   },
   {
     timestamps: {
